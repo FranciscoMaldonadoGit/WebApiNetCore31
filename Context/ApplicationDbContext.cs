63e2ef254caf0bb40ea40ab971927424045f4973
FranciscoMaldonadoGit <maldonado188francisco@gmail.com>
2020-11-02 12:36:36 -0600

    instalando entity framewok core, preparando los modelos y el dbcontext

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ApiLibros.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLibros.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { 
+        
+        }
+
+        public DbSet<Autor> Autores;
+
+    }
+}
---
+++ Controllers/AutorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLibros.Controllers
+{
+    [Route("api/[Controller]")] //api/Autor
+    [ApiController]
+    public class AutorController: ControllerBase
+    {
+
+    }
+}
